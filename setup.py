b87ef9d97c0a59d61f56fd10bd8ad3821f0eb6b3
Curtis Maloney <curtis@tinbrain.net>
2017-05-15 21:03:46 +1000

    Bump version to 1.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='labeled-enum',
-    version='1.0.0',
+    version='1.0.1',
     description='Django friendly, iterable Enum type with labels.',
     author='Curtis Maloney',
     author_email='curtis@tinbrain.net',
