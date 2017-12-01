4c31dd36bb141cf271856f924fb80cddabbd729e
Curtis Maloney <curtis@tinbrain.net>
2017-12-01 11:19:41 +1100

    Bumped version to 1.0.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst') as fin:
 
 setup(
     name='labeled-enum',
-    version='1.0.4',
+    version='1.0.5',
     description='Django friendly, iterable Enum type with labels.',
     long_description=description,
     author='Curtis Maloney',
